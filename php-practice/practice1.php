f58aba298a91a591add9c75100f3ae8d92256e62
Susannah Lowe <lowesusannah@gmail.com>
2018-02-26 16:11:03 -0800

    Create first practice exercise for udemy course

---
+++ php-practice/practice1.php
@@ -0,0 +1,14 @@
+<?php
+
+$number1 = 10;
+$number2 = 20;
+
+echo $number1 + $number2;
+
+//regular array
+$regularArray = array($number1, $number2);
+
+//associative array
+$assocArray = array("first_number" => $number1, "second_number" => $number2);
+
+?>
