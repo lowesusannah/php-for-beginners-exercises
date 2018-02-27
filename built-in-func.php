fb069252f179dc319f95ebdca9f0706a1066ad36
Susannah Lowe <lowesusannah@gmail.com>
2018-02-27 15:57:29 -0800

    Create practice exercise for built in PHP

---
+++ built-in-func.php
@@ -0,0 +1,17 @@
+<?php
+
+//uses a built in php math function here and echoes it
+$find_Exponent = pow(7, 10);
+echo $find_Exponent;
+
+//uses a built in php string function here and echoes it
+$string = "I am a STRING";
+$shouting = touppercase($string);
+echo $shouting;
+
+//uses a built in php array function and echoes it
+$list = [1, 2, 3, 4];
+$largest_Number = max($list);
+echo $largest_Number;
+
+?>
